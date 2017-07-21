30bf669c6aa9d4c3f2f31bb97cf83cea9212a346
raineluntta <raine.luntta@gmail.com>
2017-07-22 02:21:49 +0300

    A lot of refactoring for pretty much everything

--- logify.js
+++ logify.js
@@ -1,32 +1,35 @@
 /*!
  * --------------------------------------------------------------------------
  * Name: LogifyJS (v1.0)
- * Authors: Raine Luntta (http://isopaha.fi)
+ * Authors: Raine Luntta (https://github.com/raineluntta)
  * URL: https://github.com/raineluntta/LogifyJS
  * License: MIT (https://github.com/raineluntta/LogifyJS/blob/master/LICENSE)
  * --------------------------------------------------------------------------
  */
 
-function Logify() {
-	this.DevMode = function(flag) {
-		if (flag == null) {
-			return JSON.parse(localStorage.getItem("DevMode")) || false;
-		} else if (typeof flag === 'boolean') {
-			localStorage.setItem("DevMode", flag);
-		} else {
-			this.error("DevMode-function only accepts boolean values");
+function Logify(options) {
+	console.log("this",this);
+	this.extend = function logifyExtend(params) {
+		params = params || {};
+		for (var i = 1; i < arguments.length; i++) {
+			var object = arguments[i];
+			if (!object)
+				continue;
+			for (var key in object) {
+				if (object.hasOwnProperty(key)) {
+					if (typeof object[key] === 'object')
+						params[key] = this.extend(params[key], object[key]);
+					else
+						params[key] = object[key];
+				}
+			}
 		}
-	}
+		return params;
+	};
 
-	this.setLogLevel = function(level) {
-		localStorage.setItem("LogLevel", Number(level));
-	}
-
-	this.getLogLevel = function() {
-		return localStorage.getItem("LogLevel") || 3;
-	}
-
-	const loglevels = {
+	this.defaults = {
+		devMode: true,
+		logLevel: 1,
 		fatal: 7,
 		bug: 6,
 		error: 5,
@@ -34,62 +37,93 @@ function Logify() {
 		security: 3,
 		todo: 2,
 		log: 1
-	};
+	}
 
-	this.log = function() {
-		if (this.DevMode() && this.getLogLevel() <= loglevels.log) {
+	this.options = options;
+
+	this.settings = this.extend({}, this.defaults, options);
+
+	this.devMode = function (flag) {
+		if (flag == null) {
+			return JSON.parse(localStorage.getItem("Logify.devMode")) || this.settings.devMode;
+		} else if (typeof flag === 'boolean') {
+			localStorage.setItem("Logify.devMode", Boolean(flag));
+		} else {
+			this.error("devMode-function only accepts boolean values");
+		}
+	}
+	this.devMode(this.settings.devMode);
+
+	this.logLevel = function (level) {
+		if (level == null) {
+			return JSON.parse(localStorage.getItem("Logify.logLevel"));
+		} else if (typeof level === 'number') {
+			localStorage.setItem("Logify.logLevel", Number(level));
+		} else {
+			this.error("logLevel-function only works with numbers.");
+		}
+	}
+	this.logLevel(this.settings.logLevel);
+
+	this.log = function logifyLog() {
+		if (this.logLevel() <= this.settings.log) {
 			Array.prototype.unshift.call(arguments, "LOG");
 			console.log.apply(console, arguments);
 		}
 	}
 
-	this.todo = function() {
-		if (this.DevMode() && this.getLogLevel() <= loglevels.todo) {
+	this.todo = function logifyTodo() {
+		if (this.logLevel() <= this.settings.todo) {
 			Array.prototype.unshift.call(arguments, "TODO");
 			console.log.apply(console, arguments);
 		}
 	}
 
-	this.security = function() {
-		if (this.DevMode() && this.getLogLevel() <= loglevels.security) {
+	this.security = function logifySecurity() {
+		if (this.logLevel() <= this.settings.security) {
 			Array.prototype.unshift.call(arguments, "SECURITY");
 			console.warn.apply(console, arguments);
 		}
 	}
 
-	this.warn = function() {
-		if (this.DevMode() && this.getLogLevel() <= loglevels.warn) {
+	this.warn = function logifyWarn() {
+		if (this.logLevel() <= this.settings.warn) {
 			Array.prototype.unshift.call(arguments, "WARNING");
 			console.warn.apply(console, arguments);
 		}
 	}
 
-	this.error = function() {
-		if (this.DevMode() && this.getLogLevel() <= loglevels.error) {
+	this.error = function logifyError() {
+		if (this.logLevel() <= this.settings.error) {
 			Array.prototype.unshift.call(arguments, "ERROR");
 			console.error.apply(console, arguments);
 		}
 	}
 
-	this.bug = function() {
-		if (this.DevMode() && this.getLogLevel() <= loglevels.bug) {
+	this.bug = function logifyBug() {
+		if (this.logLevel() <= this.settings.bug) {
 			Array.prototype.unshift.call(arguments, "BUG");
 			console.error.apply(console, arguments);
 		}
 	}
 
-	this.fatal = function() {
-		if (this.DevMode() && this.getLogLevel() <= loglevels.fatal) {
+	this.fatal = function logifyFatal() {
+		if (this.logLevel() <= this.settings.fatal) {
 			Array.prototype.unshift.call(arguments, "FATAL");
 			console.error.apply(console, arguments);
 		}
 	}
-	this.assert = function() {
-		if (this.DevMode()) {
-			console.assert.apply(console, arguments);
+
+	this.assert = function logifyAssert(condition, message, openDebugger) {
+		if (this.devMode() == true) {
+			if (condition) {
+				return;
+			}
+			if (openDebugger) {
+				debugger;
+			} else {
+				throw new Error("ASSERTION FAILED: " + message);
+			}
 		}
 	}
-
 }
-
-const logify = new Logify();
